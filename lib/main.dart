3fb21e747b56abd238aa18fe54fe66bd2824ec0d
anishkha02 <anishkha0208@gmail.com>
2023-07-21 19:28:53 +0530

    Firebase files added

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,17 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:reddify/features/auth/screens/login_screen.dart';
 import 'package:reddify/theme/pallete.dart';
-
-void main() {
-  runApp(const MyApp());
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
+void main() async{
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+);
+  runApp(const ProviderScope(
+    child: MyApp(),
+  ));
 }
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
